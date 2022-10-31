6e4adeeb5c4df0c70146a521a5e03de5a4d9aa34
WilliamA1pha <s3864077@rmit.edu.vn>
2022-10-31 11:56:36 +0700

    update style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Container>
-        <ListProduct></ListProduct>
+        {/* <ListProduct></ListProduct> */}
         <ListCategory></ListCategory>
         <AddForm></AddForm>
       </Container>
--- client/src/components/ListCategory.jsx
+++ client/src/components/ListCategory.jsx
@@ -1,8 +1,20 @@
+import {
+  List,
+  ListItemText,
+  ListSubheader,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 function ListCategory() {
-  const [product, setProduct] = useState(null);
+  const [data, setProduct] = useState(null);
 
   useEffect(() => {
     axios
@@ -11,10 +23,36 @@ function ListCategory() {
   }, []);
   return (
     <>
-      <h1> Product: </h1>
-      <section>
-        {product && product.map((category) => <h1>{category.name}</h1>)}
-      </section>
+      <Typography variant="h3"> Category: </Typography>
+
+      {data &&
+        data.map((category) => (
+          <>
+            <Typography variant="h4">{category.name}</Typography>
+
+            <List
+              sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+              component="nav"
+              aria-labelledby="nested-list-subheader"
+            >
+              {category.subCategory?.map((sub) => (
+                <Box>
+                  <Typography variant="h6">{sub.name}</Typography>
+                  <Box sx={{ border: 1 }}>
+                    {sub.productList?.map((prod) => (
+                      <ListItemText variant="p">{prod.name}</ListItemText>
+                    ))}
+                  </Box>
+                </Box>
+              ))}
+              <Box sx={{ border: 1 }}>
+                {category.productList?.map((prod) => (
+                  <ListItemText variant="p">{prod.name}</ListItemText>
+                ))}
+              </Box>
+            </List>
+          </>
+        ))}
     </>
   );
 }
